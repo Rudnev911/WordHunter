3b78af1b40a5d3b5462ac23c6a464de1a0f6547d
rudnev <ilyarudnev16@gmail.com>
2024-11-13 21:01:13 +0300

    Проект с предварительными настройками

---
+++ app/src/main/java/com/example/wordhunter/LoginActivity.kt
@@ -0,0 +1,20 @@
+package com.example.wordhunter
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.wordhunter.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding // Объявление свойства binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+
+    }
+}
---
+++ app/src/main/java/com/example/wordhunter/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.wordhunter
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        if (FirebaseAuth.getInstance().currentUser == null) {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+    }
+
+}
